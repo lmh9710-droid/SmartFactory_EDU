afc06737e5040104e1998f458b495d58edd2b656
I am nothing <lmh9710@gmail.com>
2026-09-04 11:28:19 +0900

    Update comments and class descriptions in SensorReader

--- DataCollector/middleware/SensorReader.h
+++ DataCollector/middleware/SensorReader.h
@@ -3,15 +3,16 @@
 #include <windows.h>
 #include "DatabaseManager.h"
 
-// 1. 최상위 기본 센서 리더 (공통 시리얼 로직 포함)
+/* 1. 최상위 기본 센서 리더 (공통 시리얼 로직 포함) */
 class SensorReader {
-protected:
+ 
+ protected:
     HANDLE hSerial;
     std::string port_name;
     int baud_rate;
-    DatabaseManager& db_manager; // 다형성: 자식 DB 매니저들을 부모형태로 안전하게 참조받음
+    DatabaseManager& db_manager; /* 자식 DB 매니저들을 부모형태로 안전하게 참조받음 */
 
-public:
+ public:
     SensorReader(std::string port, int baud, DatabaseManager& db);
     virtual ~SensorReader();
     
@@ -19,20 +20,20 @@ public:
     void start_listening();
     void close_port();
     
-    // 자식 클래스들이 데이터 수신 시 화면 출력이나 전처리를 다르게 하도록 가상함수 정의
+    /* 자식 클래스들이 데이터 수신 시 화면 출력이나 전처리를 다르게 하도록 가상함수 정의 */
     virtual void handle_received_line(std::string line) = 0;
 };
 
-// 2. [자식] 온습도 전용 리더 (COM10용)
+/* 2. [자식] 온습도 전용 리더 (COM11용) */
 class DHTReader : public SensorReader {
-public:
+  public:
     DHTReader(std::string port, int baud, DatabaseManager& db);
     void handle_received_line(std::string line) override;
 };
 
-// 3. [자식] 광도 전용 리더 (COM12용)
+/* 3. [자식] 광도 전용 리더 (COM12용) */
 class LightReader : public SensorReader {
-public:
+  public:
     LightReader(std::string port, int baud, DatabaseManager& db);
     void handle_received_line(std::string line) override;
 };
